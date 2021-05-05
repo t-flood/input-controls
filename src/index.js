e756c140372ff7077241af1b1f8985396d52fa6f
Tim Flood <tflood9@gmail.com>
2021-05-05 16:27:21 -0400

    display each input in real time

--- src/index.js
+++ src/index.js
@@ -1,15 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Input extends Component {
+  state = { value: "" };
+  handleChange = (event) => {
+    this.setState({ value: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+  };
+
+  render() {
+    return (
+      <>
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Practice:
+              <input
+                type="text"
+                value={this.state.value}
+                onChange={this.handleChange}
+              ></input>
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+          <p>{this.state.value}</p>
+        </div>
+      </>
+    );
+  }
+}
+
+ReactDOM.render(<Input />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
